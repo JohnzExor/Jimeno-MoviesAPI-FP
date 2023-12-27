ecf4b2757be8d40a8e9c92bfb73f23268d4ee6ab
JohnzExor <jimenojohnzyll@gmail.com>
2023-12-27 10:11:25 +0800

    fixed some style and optimizes script for more readability

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const container = document.getElementById("container");
 const form = document.getElementById("form");
 const details = document.getElementById("details");
+const searchValue = document.getElementById("searchValue");
 
 const apiKey = "190459cb7522c147260473041d9226ca";
 const webLink = "http://image.tmdb.org/t/p/w500/";
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const value = document.getElementById("searchValue").value;
-  fetchSearchMovie(value);
+  fetchSearchMovie(searchValue.value);
 });
 
 const moviesTemplate = (id, backdrop_path, original_title) => {
@@ -18,24 +18,41 @@ const moviesTemplate = (id, backdrop_path, original_title) => {
           </button>`;
 };
 
+const movieDetailsTemplate = (
+  backdrop_path,
+  genres,
+  original_title,
+  release_date,
+  overview
+) => {
+  return `<div class=movie-details>
+            <img src=${webLink}${backdrop_path} class=movie-details-poster>
+            <span>
+              <p>${genres.map(({ name }) => name).join(" - ")}</p>
+              <h1>${original_title}</h1>
+              <p>Release Date: ${release_date}</p>
+              <p style=font-weight:bold>Overview: <br><span style=font-weight: normal>${overview}</span></p>
+            </span>
+          </div>`;
+};
+
 const fetchUpComingMovies = async () => {
   try {
     const response = await fetch(
       `https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&language=en-US&page=1`
     );
-    const data = await response.json();
+    const { results } = await response.json();
 
     container.innerHTML = `
     <div class=movies-page>
       <h1 class=page-title>Upcoming Movies<h1>
       <div class=movies>
-         ${data.results
+         ${results
            .map(({ id, backdrop_path, original_title }) =>
              moviesTemplate(id, backdrop_path, original_title)
            )
            .join("")}
       </div>
-   
     </div>`;
   } catch (error) {
     console.log(error.message);
@@ -47,18 +64,17 @@ const fetchSearchMovie = async (name) => {
     const response = await fetch(
       `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${name}`
     );
-    const data = await response.json();
+    const { results } = await response.json();
     container.innerHTML = `
     <div class=movies-page>
       <h1 class=page-title>You searched for ${name}<h1>
       <div class=movies>
-        ${data.results
+        ${results
           .map(({ id, backdrop_path, original_title }) =>
             moviesTemplate(id, backdrop_path, original_title)
           )
           .join("")}
       </div>
-
     </div>`;
   } catch (error) {
     console.log(error.message);
@@ -78,16 +94,14 @@ const fetchMovieDetails = async (id) => {
     const similarData = await similarResponse.json();
 
     container.innerHTML = `
-      <div class=movie-details-tab>
-        <div class=movie-details>
-          <img src=${webLink}${backdrop_path} class=movie-details-poster>
-          <span>
-            <p>${genres.map(({ name }) => name).join(" - ")}</p>
-            <h1>${original_title}</h1>
-            <p>Release Date: ${release_date}</p>
-            <p style=font-weight:bold>Overview: <br><span style=font-weight: normal>${overview}</span></p>
-          </span>
-        </div>
+      <div class=movies-page>
+      ${movieDetailsTemplate(
+        backdrop_path,
+        genres,
+        original_title,
+        release_date,
+        overview
+      )}
         <h1 class=page-title>You may also like</h1>
         <div class=movies>
           ${similarData.results
